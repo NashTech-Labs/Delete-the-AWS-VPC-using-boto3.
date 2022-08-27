ad2b84805bf232e94b3a26205d5b8a4fc11c14e2
MuZakkir Saifi <mohd.saifi@knoldus.com>
2022-08-28 01:49:49 +0530

    Update in code

--- delete_VPC.py
+++ delete_VPC.py
@@ -1,3 +1,4 @@
+# By MuZakkir Saifi
 # import logging for get the logs in  execution
 import logging
 # import the boto3 which will use to interact  with the aws
@@ -5,30 +6,30 @@ import boto3
 from botocore.exceptions import ClientError
 import json
 
-AWS_REGION = input("Please enter the AWS_REGION")
+REGION = input("Please enter the REGION")
 
-# this is the configration for the logger
+# this is the configration for the logger_for
 
-logger = logging.getLogger()
+logger_for = logging.getLogger()
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s: %(levelname)s: %(message)s')
 
-vpc_client = boto3.client("ec2", region_name=AWS_REGION)
+vpc_responce = boto3.client("ec2", region_name=REGION)
 
 
-def delete_vpc(vpc_id):
+def delete(vpc_id):
     
     try:
-        response = vpc_client.delete_vpc(VpcId=vpc_id)
+        res = vpc_responce.delete_vpc(VpcId=vpc_id)
 
     except ClientError:
-        logger.exception('This Could not delete the vpc.')
+        logger_for.exception('Oops sorry, Your VPC can not be deleted.')
         raise
     else:
-        return response
+        return res
 
 
 if __name__ == '__main__':
-    VPC_ID = input("Please enter the VPC ID")
-    vpc = delete_vpc(VPC_ID)
-    logger.info(f'Wow, Your VPC {VPC_ID} is deleted successfully.')
+    ID = input("Please enter the VPC ID")
+    vpc = delete(ID)
+    logger_for.info(f'Wow, Your VPC {ID} is deleted successfully.')
